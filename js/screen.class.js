ad3b228b5a042fd03b54bf8d97752ac60adbc8a0
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2018-01-06 18:44:15 +0000

    adds full screen listener

--- js/screen.class.js
+++ js/screen.class.js
@@ -8,15 +8,32 @@
 class Screen {
 
     constructor() {
+        this._fullScreenBtn = $('#toggleFullScreen');
         this.initToggleScreenBtn();
+        this.initFullscreenListener();
     }
 
     initToggleScreenBtn() {
         var _this = this;
-        $('#toggleFullScreen').on('click', function(event, target){
+        this._fullScreenBtn.on('click', function(event, target){
             _this.toggleFullscreen(document.documentElement);
-            target.hide();
         });
+    }
+
+    initFullscreenListener() {
+        var _this = this;
+        var fullScreenHandler = function () {
+            if (document.webkitIsFullScreen == true || document.mozFullScreen == true || document.msFullscreenElement == true) {
+                _this._fullScreenBtn.hide();
+                return;
+            }
+            _this._fullScreenBtn.show();
+        }
+        
+        document.addEventListener('webkitfullscreenchange', fullScreenHandler, false);
+        document.addEventListener('mozfullscreenchange', fullScreenHandler, false);
+        document.addEventListener('fullscreenchange', fullScreenHandler, false);
+        document.addEventListener('MSFullscreenChange', fullScreenHandler, false);
     }
 
     toggleFullscreen(element) {
